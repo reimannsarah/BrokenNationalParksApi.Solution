bed968a8532175b716bc682ffc8014dd25456122
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-09 08:45:46 -0700

    change file names from generated file names

---
+++ NationalParksApi/Controllers/NatlParksController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NationalParksApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class NatlParksController : ControllerBase
+{
+
+}
---
+++ NationalParksApi/Models/NatlPark.cs
@@ -0,0 +1,9 @@
+namespace NationalParksApi;
+
+public class NatlPark
+{
+    public int NatlParkId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int StateId { get; set; }
+}
