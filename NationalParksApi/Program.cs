e60605c3434e7802a0d50dacf2b3da136910e5c8
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-09 11:15:38 -0700

    add versioning configuration

--- NationalParksApi/Program.cs
+++ NationalParksApi/Program.cs
@@ -19,9 +19,30 @@ builder.Services.AddDbContext<NationalParksApiContext>(
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddApiVersioning(opt =>
+                                    {
+                                        opt.DefaultApiVersion = new ApiVersion(1, 0);
+                                        opt.AssumeDefaultVersionWhenUnspecified = true;
+                                        opt.ReportApiVersions = true;
+                                        opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                                        new HeaderApiVersionReader("x-api-version"),
+                                                                                        new MediaTypeApiVersionReader("x-api-version"));
+                                    });
 
-// Configure the HTTP request pipeline.
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -32,6 +53,11 @@ else
     app.UseHttpsRedirection();
 }
 
+app.UseRequestLocalization(options);
+app.UseStaticFiles();
+app.UseMiddleware<LocalizerMiddleware>();
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -39,3 +65,64 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public class ConfigureSwaggerOptions
+    : IConfigureNamedOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider _provider;
+
+    public ConfigureSwaggerOptions(
+        IApiVersionDescriptionProvider provider)
+    {
+        _provider = provider;
+    }
+
+    /// <summary>
+    /// Configure each API discovered for Swagger Documentation
+    /// </summary>
+    /// <param name="options"></param>
+    public void Configure(SwaggerGenOptions options)
+    {
+        // add swagger document for every API version discovered
+        foreach (var description in _provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(
+                description.GroupName,
+                CreateVersionInfo(description));
+        }
+    }
+
+    /// <summary>
+    /// Configure Swagger Options. Inherited from the Interface
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="options"></param>
+    public void Configure(string name, SwaggerGenOptions options)
+    {
+        Configure(options);
+    }
+
+    /// <summary>
+    /// Create information about the version of the API
+    /// </summary>
+    /// <param name="description"></param>
+    /// <returns>Information about the API</returns>
+    private OpenApiInfo CreateVersionInfo(
+            ApiVersionDescription desc)
+    {
+        var info = new OpenApiInfo()
+        {
+            Title = ".NET Core (.NET 6) Web API",
+            Version = desc.ApiVersion.ToString()
+        };
+
+        if (desc.IsDeprecated)
+        {
+            info.Description += " This API version has been deprecated. Please use one of the new APIs available from the explorer.";
+        }
+
+        return info;
+    }
+}
+
+
