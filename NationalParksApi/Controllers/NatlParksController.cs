2941edaa30c3ee131ea188bf6e55c12e05713be3
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-09 10:26:39 -0700

    add Get route for all NatlParks

--- NationalParksApi/Controllers/NatlParksController.cs
+++ NationalParksApi/Controllers/NatlParksController.cs
@@ -1,10 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using NationalParksApi.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace NationalParksApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class NatlParksController : ControllerBase
 {
+  private readonly NationalParksApiContext _db;
 
+  public NatlParksController(NationalParksApiContext db)
+  {
+    _db = db;
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<NatlPark>>> GetNatlParks()
+  {
+    return await _db.NatlParks.ToListAsync();
+  }
 }
