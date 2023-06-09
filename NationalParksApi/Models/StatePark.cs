b60ad88d8f9acb161b0a41e490906c401a5d6c98
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-09 09:01:54 -0700

    Fix typo

--- NationalParksApi/Models/StatePark.cs
+++ NationalParksApi/Models/StatePark.cs
@@ -2,7 +2,7 @@ namespace NationalParksApi;
 
 public class StatePark
 {
-  public int NatlParkId { get; set; }
+  public int StateParkId { get; set; }
   public string Name { get; set; }
   public string Description { get; set; }
   public int StateId { get; }
